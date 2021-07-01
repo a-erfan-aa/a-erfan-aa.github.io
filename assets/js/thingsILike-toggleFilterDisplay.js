31d170f519cfc1d702cf9c895d8ea995743dd3d0
a-erfan-aa <erfanaszman@gmail.com>
2021-07-01 22:05:23 +0800

    various changes

--- assets/js/thingsILike-toggleFilterDisplay.js
+++ assets/js/thingsILike-toggleFilterDisplay.js
@@ -3,11 +3,11 @@
 function toggleFilterDisp() {
 	var fToggle = document.getElementById("filterToggle");
 	var fDisp = document.getElementById("filters");
-	if (fToggle.innerHTML === "click to hide filters") {
-		fToggle.innerHTML = "click to show filters";
+	if (fToggle.innerHTML === "click/tap to hide filters") {
+		fToggle.innerHTML = "click/tap to show filters";
 		fDisp.style.display = "none";
 	} else {
-		fToggle.innerHTML = "click to hide filters";
+		fToggle.innerHTML = "click/tap to hide filters";
 		fDisp.style.display = "block";
 	}
 }
