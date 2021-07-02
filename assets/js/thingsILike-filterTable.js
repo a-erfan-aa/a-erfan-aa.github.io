d8a9734c9bb8b43851333112af8e1c6c9ce4f78d
a-erfan-aa <erfanaszman@gmail.com>
2021-07-02 09:20:34 +0800

    cleanup
    got rid of a class i didnt need anymore
    
    got rid of a redundant if() in the js
    
    other small changes

--- assets/js/thingsILike-filterTable.js
+++ assets/js/thingsILike-filterTable.js
@@ -18,25 +18,18 @@ function filterTable() {
 	
 	//the filtering
 	//note to self: i starts at 1 because tr[0] is the column headers and those are always shown
-	if (checkedCount == 0) {
-		//If no checkboxes are ticked, show all
-		for (i = 1; i < tr.length; i++) {
-			tr[i].style.display = "";
+	//check if a <tr> has number of filters matching number of checked. if yes, show. if not, hide. then do it all again til last <tr>.
+	for (i = 1; i < tr.length; i++) {
+		filterMatchCount = 0;
+		{% for item in site.data.thingsILike-filters %}
+		if ({{ item.name }}.checked == true) {
+			if (tr[i].className.indexOf('{{ item.name }}') > -1) { filterMatchCount++; }
 		}
-	} else {
-		//else, check if a <tr> has number of filters matching number of checked. if yes, show. if not, hide. then do it all again til last <tr>.
-		for (i = 1; i < tr.length; i++) {
-			filterMatchCount = 0;
-			{% for item in site.data.thingsILike-filters %}
-			if ({{ item.name }}.checked == true) {
-				if (tr[i].className.indexOf('{{ item.name }}') > -1) { filterMatchCount++; }
-			}
-			{% endfor %}
-			if (checkedCount == filterMatchCount) {
-				tr[i].style.display = "";
-			} else {
-				tr[i].style.display = "none";
-			}
+		{% endfor %}
+		if (checkedCount == filterMatchCount) {
+			tr[i].style.display = "";
+		} else {
+			tr[i].style.display = "none";
 		}
 	}
 }
