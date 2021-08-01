a3650d803f81f8037135b035fe95982e421fcb56
a-erfan-aa <erfanaszman@gmail.com>
2021-08-02 00:45:57 +0800

    rewrite thingsILike-filterTable.js so jekyll generates prettier source
    used to make a lot of whitespace when jekyll builds the .js file
    
    used some && to make code smaller and less indenty
    
    also got rid of some redundant code at line 10. seriously what was going through my head at the time.

--- assets/js/thingsILike-filterTable.js
+++ assets/js/thingsILike-filterTable.js
@@ -7,25 +7,17 @@ function filterTable() {
 	var table = document.getElementById("theTable");
 	var tr = table.getElementsByTagName("tr");
 	var i;
-	//filter checkbox vars
-	{% for item in site.data.thingsILike-filters %}
-	var {{ item.name }}Check = document.getElementById("{{ item.name }}");
-	{% endfor %}
-	//track how many boxes are ticked
-	{% for item in site.data.thingsILike-filters %}
-	if ({{ item.name }}.checked == true) { checkedCount++; }
-	{% endfor %}
+	
+	//track how many boxes are ticked {% for item in site.data.thingsILike-filters %}
+	if ({{ item.name }}.checked == true) { checkedCount++; }{% endfor %}
 	
 	//the filtering
 	//note to self: i starts at 1 because tr[0] is the column headers and those are always shown
 	//check if a <tr> has number of filters matching number of checked. if yes, show. if not, hide. then do it all again til last <tr>.
 	for (i = 1; i < tr.length; i++) {
-		filterMatchCount = 0;
-		{% for item in site.data.thingsILike-filters %}
-		if ({{ item.name }}.checked == true) {
-			if (tr[i].className.indexOf('{{ item.name }}') > -1) { filterMatchCount++; }
-		}
-		{% endfor %}
+		filterMatchCount = 0; {% for item in site.data.thingsILike-filters %}
+		if ({{ item.name }}.checked == true && tr[i].className.indexOf('{{ item.name }}') > -1) { filterMatchCount++; }{% endfor %}
+		
 		if (checkedCount == filterMatchCount) {
 			tr[i].style.display = "";
 		} else {
