0e8f24890340d0a1f683abfae8895d3d1a77d8ae
a-erfan-aa <erfanaszman@gmail.com>
2021-08-05 02:23:58 +0800

    add better spacing to comments in a couple files

--- assets/js/thingsILike-filterTable.js
+++ assets/js/thingsILike-filterTable.js
@@ -1,19 +1,19 @@
 ---
 ---
 function filterTable() {
-	//vars
+	// vars
 	var checkedCount = 0;
 	var filterMatchCount = 0;
 	var table = document.getElementById("theTable");
 	var tr = table.getElementsByTagName("tr");
 	var i;
 	
-	//track how many boxes are ticked {% for item in site.data.thingsILike-filters %}
+	// track how many boxes are ticked {% for item in site.data.thingsILike-filters %}
 	if ({{ item.name }}.checked == true) { checkedCount++; }{% endfor %}
 	
-	//the filtering
-	//note to self: i starts at 1 because tr[0] is the column headers and those are always shown
-	//check if a <tr> has number of filters matching number of checked. if yes, show. if not, hide. then do it all again til last <tr>.
+	// the filtering
+	// note to self: i starts at 1 because tr[0] is the column headers and those are always shown
+	// check if a <tr> has number of filters matching number of checked. if yes, show. if not, hide. then do it all again til last <tr>.
 	for (i = 1; i < tr.length; i++) {
 		filterMatchCount = 0; {% for item in site.data.thingsILike-filters %}
 		if ({{ item.name }}.checked == true && tr[i].className.indexOf('{{ item.name }}') > -1) { filterMatchCount++; }{% endfor %}
